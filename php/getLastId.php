c4003e645bf11eef39ea281db2fb1d4497881045
Fernan <lfortega0625@gmail.com>
2026-05-04 23:07:14 -0400

    fix(reserve id): get last ID finished

--- php/getLastId.php
+++ php/getLastId.php
@@ -11,10 +11,6 @@ if(!file_exists($FILE_PATH)){
 
 $FILE = fopen($FILE_PATH, 'r') or die('Unable to open file!');
 
-$titles = fgets($FILE);
-$arrayKeys = explode("; ", $titles);
-
-
 function getNewLine($FILE, $pos){
     do{
         $pos-=1;
@@ -29,27 +25,30 @@ function getNewLine($FILE, $pos){
 }
 
 
-$pos = -1; // SEEK_END solo acepta valores negativos
-$arrayCombined = ["modification"=> ""];
+$arrayValues = [];
 
-while($arrayCombined["modification"] != "00"){ // 00 - resgistro sin actualizaciones para ultimo ID de la ficha 
+$pos = -1; // SEEK_END solo acepta valores negativos
+
+while( ($arrayValues[4] ?? NULL) != "00" ){ // 00 - resgistro sin actualizaciones para ultimo ID de la ficha 
     
     [$currentLine, $newPos] = getNewLine($FILE, $pos);
 
-    if($currentLine){
-        $arrayValues = explode("; ", $currentLine);
-        $arrayCombined = array_combine($arrayKeys, $arrayValues);
+    if(count($arrayValues) == 1){ // si el array tiene solo 1 elemento (el codigo), detener el while
+        break;
     }
 
+    $arrayValues = explode("; ", $currentLine);
+    
     $pos = (int)$newPos-1; // ultima posicion(\n) -1 para saltar la posicion del (\n) y subir a la siguiente linea
 }
 
 
-$ID_key = $arrayKeys[0];
-$ID = $arrayCombined["cod_n"];
+$ID = $arrayValues[0];
 
 $ID = (int)$ID +1; // incrementa el ID en +1 para obtener el nuevo ID para la ficha
 $ID = str_pad($ID, 6, '0', STR_PAD_LEFT); // rellenar el ID con ceros para formato de 6 digitos
 
-echo json_encode([$ID_key => $ID]);
+file_put_contents($FILE_PATH, "\n{$ID}", FILE_APPEND);
+
+echo json_encode(["cod_n" => $ID]);
 
